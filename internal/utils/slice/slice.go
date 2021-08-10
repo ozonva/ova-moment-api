74176b1c367d11874053082f607c970da7b0797d
dotmkey <dotmkey@gmail.com>
2021-08-10 19:03:38 +0300

    slice.DiffInt and slice.DiffStr functions + tests

--- internal/utils/slice/slice.go
+++ internal/utils/slice/slice.go
@@ -1,5 +1,9 @@
 package slice
 
+import (
+	"github.com/ozonva/ova-moment-api/internal/utils"
+)
+
 func ChunksInt(slice []int, chunkSize int) [][]int {
 	if chunkSize <= 0 {
 		panic("Chunk size must be a positive integer")
@@ -7,7 +11,7 @@ func ChunksInt(slice []int, chunkSize int) [][]int {
 
 	chunks := make([][]int, (len(slice)+chunkSize-1)/chunkSize)
 	for i := 0; i < len(chunks); i++ {
-		chunks[i] = slice[i*chunkSize : minInt(i*chunkSize+chunkSize, len(slice))]
+		chunks[i] = slice[i*chunkSize : utils.MinInt(i*chunkSize+chunkSize, len(slice))]
 	}
 
 	return chunks
@@ -20,15 +24,42 @@ func ChunksStr(slice []string, chunkSize int) [][]string {
 
 	chunks := make([][]string, (len(slice)+chunkSize-1)/chunkSize)
 	for i := 0; i < len(chunks); i++ {
-		chunks[i] = slice[i*chunkSize : minInt(i*chunkSize+chunkSize, len(slice))]
+		chunks[i] = slice[i*chunkSize : utils.MinInt(i*chunkSize+chunkSize, len(slice))]
 	}
 
 	return chunks
 }
 
-func minInt(a int, b int) int {
-	if a <= b {
-		return a
+func DiffInt(minuend []int, subtrahend ...int) []int {
+	var stub struct{}
+	subtrahendMap := make(map[int]struct{}, len(subtrahend))
+	for _, v := range subtrahend {
+		subtrahendMap[v] = stub
 	}
-	return b
+
+	difference := make([]int, 0)
+	for _, v := range minuend {
+		if _, found := subtrahendMap[v]; !found {
+			difference = append(difference, v)
+		}
+	}
+
+	return difference
+}
+
+func DiffStr(minuend []string, subtrahend ...string) []string {
+	var stub struct{}
+	subtrahendMap := make(map[string]struct{}, len(subtrahend))
+	for _, v := range subtrahend {
+		subtrahendMap[v] = stub
+	}
+
+	difference := make([]string, 0)
+	for _, v := range minuend {
+		if _, found := subtrahendMap[v]; !found {
+			difference = append(difference, v)
+		}
+	}
+
+	return difference
 }
--- internal/utils/slice/slice_test.go
+++ internal/utils/slice/slice_test.go
@@ -18,7 +18,7 @@ func TestChunksInt(t *testing.T) {
 		error     string
 	}
 
-	var successCases = []successCase{
+	successCases := []successCase{
 		{
 			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14},
 			4,
@@ -36,7 +36,7 @@ func TestChunksInt(t *testing.T) {
 		},
 	}
 
-	var panicCases = []panicCase{
+	panicCases := []panicCase{
 		{[]int{1, 2, 3, 4, 5}, 0, "Chunk size must be a positive integer"},
 		{[]int{}, -2, "Chunk size must be a positive integer"},
 	}
@@ -66,7 +66,7 @@ func TestChunksStr(t *testing.T) {
 		error     string
 	}
 
-	var successCases = []successCase{
+	successCases := []successCase{
 		{
 			[]string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n"},
 			4,
@@ -84,7 +84,7 @@ func TestChunksStr(t *testing.T) {
 		},
 	}
 
-	var panicCases = []panicCase{
+	panicCases := []panicCase{
 		{[]string{"a", "b", "c", "d", "e"}, 0, "Chunk size must be a positive integer"},
 		{[]string{}, -2, "Chunk size must be a positive integer"},
 	}
@@ -98,5 +98,77 @@ func TestChunksStr(t *testing.T) {
 		assert.PanicsWithValue(t, panicCase.error, func() {
 			ChunksStr(panicCase.slice, panicCase.chunkSize)
 		}, "The two errors must be same")
+	}
+}
+
+func TestDiffInt(t *testing.T) {
+	type successCase struct {
+		minuend    []int
+		subtrahend []int
+		difference []int
+	}
+
+	successCases := []successCase{
+		{
+			[]int{5, 1, 2, 8, 0, 1, 1, 3, 9, 0, 12},
+			[]int{1, 13, 6, 5, 5},
+			[]int{2, 8, 0, 3, 9, 0, 12},
+		},
+		{
+			[]int{5, 1, 2, 8, 0, 1, 1, 3, 9, 0, 12},
+			[]int{1, 13, 6, 5, 5, 8, 2, 0, 12, 9, 3, 9},
+			[]int{},
+		},
+		{
+			[]int{5, 1, 2, 8, 0, 1, 1, 3, 9, 0, 12},
+			[]int{24, 25},
+			[]int{5, 1, 2, 8, 0, 1, 1, 3, 9, 0, 12},
+		},
+		{
+			[]int{5, 1, 2, 8, 0, 1, 1, 3, 9, 0, 12},
+			[]int{},
+			[]int{5, 1, 2, 8, 0, 1, 1, 3, 9, 0, 12},
+		},
+	}
+
+	for _, successCase := range successCases {
+		res := DiffInt(successCase.minuend, successCase.subtrahend...)
+		assert.Equal(t, successCase.difference, res, "The two slices must be same")
+	}
+}
+
+func TestDiffStr(t *testing.T) {
+	type successCase struct {
+		minuend    []string
+		subtrahend []string
+		difference []string
+	}
+
+	successCases := []successCase{
+		{
+			[]string{"f", "b", "c", "i", "a", "b", "b", "d", "j", "a", "m"},
+			[]string{"b", "n", "g", "f", "f"},
+			[]string{"c", "i", "a", "d", "j", "a", "m"},
+		},
+		{
+			[]string{"f", "b", "c", "i", "a", "b", "b", "d", "j", "a", "m"},
+			[]string{"b", "n", "g", "f", "f", "i", "c", "a", "m", "j", "d", "j"},
+			[]string{},
+		},
+		{
+			[]string{"f", "b", "c", "i", "a", "b", "b", "d", "j", "a", "m"},
+			[]string{"y", "z"},
+			[]string{"f", "b", "c", "i", "a", "b", "b", "d", "j", "a", "m"},
+		},
+		{
+			[]string{"f", "b", "c", "i", "a", "b", "b", "d", "j", "a", "m"},
+			[]string{},
+			[]string{"f", "b", "c", "i", "a", "b", "b", "d", "j", "a", "m"},
+		},
+	}
+
+	for _, successCase := range successCases {
+		res := DiffStr(successCase.minuend, successCase.subtrahend...)
+		assert.Equal(t, successCase.difference, res, "The two slices must be same")
 	}
 }
