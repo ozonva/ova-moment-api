a3fe42ca5800f50a14b60d78a420417681cdca7b
dotmkey <dotmkey@gmail.com>
2021-08-14 17:17:52 +0300

    moment collection with Chunks and ToMap methods

--- internal/domain/model/moment.go
+++ internal/domain/model/moment.go
@@ -1,13 +1,16 @@
 package model
 
-import "time"
+import (
+	"github.com/ozonva/ova-moment-api/internal/utils"
+	"time"
+)
 
 type MomentID struct {
 	domainObjectID
 }
 
-func NewMomentID(ID string) *MomentID {
-	return &MomentID{domainObjectID: newDomainObjectID(ID)}
+func NewMomentID(rawID string) *MomentID {
+	return &MomentID{domainObjectID: newDomainObjectID(rawID)}
 }
 
 type MomentRepository interface {
@@ -16,7 +19,7 @@ type MomentRepository interface {
 }
 
 type Moment struct {
-	_ID         MomentID
+	momentID    MomentID
 	userID      UserID
 	occurredAt  time.Time
 	name        string
@@ -24,12 +27,12 @@ type Moment struct {
 	tags        []string
 }
 
-func NewMoment(ID MomentID, userID UserID, name string, description string) *Moment {
-	return &Moment{_ID: ID, userID: userID, occurredAt: time.Now(), name: name, description: description}
+func NewMoment(momentID MomentID, userID UserID, name string, description string) *Moment {
+	return &Moment{momentID: momentID, userID: userID, occurredAt: time.Now(), name: name, description: description}
 }
 
 func (m Moment) ID() MomentID {
-	return m._ID
+	return m.momentID
 }
 
 func (m Moment) UserID() UserID {
@@ -50,4 +53,28 @@ func (m Moment) Description() string {
 
 func (m Moment) Tags() []string {
 	return m.tags
+}
+
+type MomentCollection []Moment
+
+func (mc MomentCollection) Chunks(chunkSize int) [][]Moment {
+	if chunkSize <= 0 {
+		panic("Chunk size must be a positive integer")
+	}
+
+	chunks := make([][]Moment, (len(mc)+chunkSize-1)/chunkSize)
+	for i := 0; i < len(chunks); i++ {
+		chunks[i] = mc[i*chunkSize : utils.MinInt(i*chunkSize+chunkSize, len(mc))]
+	}
+
+	return chunks
+}
+
+func (mc MomentCollection) ToMap() map[MomentID]Moment {
+	m := make(map[MomentID]Moment)
+	for _, v := range mc {
+		m[v.ID()] = v
+	}
+
+	return m
 }
